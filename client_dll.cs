8997d5a94f3d97684a1cc9d5087ff03b924d7619
elvv47 <122207833+elvv47@users.noreply.github.com>
2026-05-02 16:37:18 +0300

    Update timestamp in client_dll.cs header

--- client_dll.cs
+++ client_dll.cs
@@ -1,5 +1,5 @@
 // Generated using https://github.com/a2x/cs2-dumper
-// 2026-04-30 23:05:47.043465 UTC
+// 2026-05-02 13:36:47.526696800 UTC
 
 namespace CS2Dumper.Schemas {
     // Module: client.dll
